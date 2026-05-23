540370d47244c2bfbcf402dbda37812342ff1eab
Israt-Nisha <isratnisha1999@gmail.com>
2026-05-23 20:46:03 +0600

    pagination fun add

--- index.js
+++ index.js
@@ -186,6 +186,11 @@ function countOccurrences(s, c) {
   return count;
 }
 
+function pagination(items, pageSize, pageNumber) {
+  const startIndex = (pageNumber - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  return items.slice(startIndex, endIndex);
+}
 
 
 
