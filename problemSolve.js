fed9745e22bcfdfe9d7a432724d2314f81bb0a65
Israt-Nisha <isratnisha1999@gmail.com>
2026-06-14 09:03:43 +0600

    isEven problem solve

--- problemSolve.js
+++ problemSolve.js
@@ -5,3 +5,22 @@ function swap(a,b) {
 
 let result = swap(5, 10);
 console.log(result);
+
+// Problem 2: Check Even or Odd  [Easy]
+// Description: Write a function isEven(n) that returns true if a number is even, and false if it is odd.
+// Example:
+// Input: 4  → Output: trueInput: 7  → Output: false
+// Hint: Use the modulus (%) operator.
+
+
+function isEven(n) {
+  
+    if (n % 2 === 0) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+    
+}
+
+isEven(10);
