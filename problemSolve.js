2f2a7ee35f981c731d5747530a9bea025c39221f
Israt-Nisha <isratnisha1999@gmail.com>
2026-06-19 22:12:20 +0600

    solve the problem removeDuplicates

--- problemSolve.js
+++ problemSolve.js
@@ -105,4 +105,18 @@ function countChar(str, char) {
     }
     return count;
 }
-console.log(countChar("banana", "a"));
+// console.log(countChar("banana", "a"));
+
+function removeDuplicates(arr) {
+  let uniqueArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!uniqueArray.includes(arr[i])) {
+      uniqueArray.push(arr[i]);
+    }
+  }
+
+  return uniqueArray;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 3, 4,5]));
